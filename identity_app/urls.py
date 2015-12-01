3de3c79147fa1db493c21b8d5584f982dd9227d2
Sugumar Venkatesan <suguvenk@gmail.com>
2015-12-01 21:07:33 +0530

    signup activation implementation

--- identity_app/urls.py
+++ identity_app/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, url
-from .views import RegistrationView,HomeView,LoginView
+from .views import RegistrationView,HomeView,LoginView,RegistrationConfirmView
 
 urlpatterns = patterns('',
     url(r'^current_task/$', HomeView.as_view(), name="current_task"),
     url(r'^register/$', RegistrationView.as_view(), name="register"),
     url(r'^login/$', LoginView.as_view(), name="login"),
+    url(r'^confirm/(?P<activation_key>\w+)/', RegistrationConfirmView.as_view() ,name="register_confirm"),
 )
 
 
